f2d57ed6608a11d99adbc47f39260e21d3966ee0
HarolM <57779449+HarolM@users.noreply.github.com>
2024-01-03 03:54:17 +0000

    Final File

--- Indoorvoice.py
+++ Indoorvoice.py
@@ -1,3 +1,5 @@
+#Use your indoor voice! A short code that translates all text to lowercase. 
+
 text = input("Enter text here: ").lower()
 
 output = text
---
+++ playbackspeed.py
@@ -0,0 +1,7 @@
+#Implement a program in Python that prompts the user for input and then outputs that same input, replacing each space with " ... "
+
+text = input("Enter text here: ").replace(" ", "...")
+
+ouput = text
+
+print(output)
